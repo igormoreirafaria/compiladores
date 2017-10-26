1a9471dcef42ac93379c3ff482edf16407118095
Igor Moreira <igormoreirafaria@gmail.com>
2017-10-26 09:40:28 -0200

    analex fix

--- analex/hello.c
+++ analex/hello.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
 int main(){
-	int b, c, d, e, t;
-	float x = 2 / (3 * 5);
-	a = (67 - 233) / e;
-	float c = 2.456 + 343.5 * 13.15;
-			/*ESCREVA QUALQUER CODIGO EM C POR FAVOR E ESPERE EU VOLTAR*/
-			return 1;
+
+	printf(x/(z - y));
+
+	return 0;
 }
